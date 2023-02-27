4322b57f989818ef25b745ac0ba1ceecec069fbd
williamdparker <parkerw@uwp.edu>
2023-02-27 16:39:56 -0600

    Finished 4.1

--- f2c_qa.py
+++ f2c_qa.py
@@ -1,3 +1,12 @@
 # Ask the user for a temperature in F and read the number
 # Compute the temperature in C and print out
 
+
+def convert_fahrenheit_to_celsius(degrees_fahrenheit):
+    return (5/9)*(degrees_fahrenheit - 32)
+
+
+input_temperature = float(input('Enter a temperature in degrees Fahrenheit: '))
+output_temperature = convert_fahrenheit_to_celsius(input_temperature)
+print(f'That temperature is {output_temperature} degrees Celsius')
+
